f0c899dda7257867d18ec2d8236fc1fddcdbf797
serendipity <fe91990@gmail.com>
2019-11-05 00:26:19 +0900

    참회록 제출 구현
    제출해야제?

--- 손지언연습용/server.js
+++ 손지언연습용/server.js
@@ -1,10 +1,46 @@
 const express=require('express');
 const app=express();
+const fs=require('fs');
+var bodyParser = require('body-parser');                                                                     
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
 
 app.get('/',(req,res)=>{
-    res.send('main.html');
+    fs.readFile('./main.html',(err,data)=>{
+        if (err)
+            throw err;
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+    });
+});
+
+app.get('/penance.html',(req,res)=>{
+    fs.readFile('./penance.html',(err,data)=>{
+        if (err)
+            throw err;
+
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+    });
+});
+
+app.post('/submit.html',(req,res)=>{
+    fs.readFile('./submit.html',(err,data)=>{
+        if (err)
+            throw err;
+        console.log(req.body.title);
+        
+        
+    
+        
+
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+    });
 });
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
 });
+
