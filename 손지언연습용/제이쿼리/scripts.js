65475f6afa9c5cfa17f28befda91119d823d8ba2
serendipity <fe91990@gmail.com>
2019-11-06 18:29:49 +0900

    제이쿼리 연습한것들
    링크 아이콘만들기, 표만들기

--- 손지언연습용/제이쿼리/scripts.js
+++ 손지언연습용/제이쿼리/scripts.js
@@ -1,1 +1,33 @@
-$(document).ready(function(){$('body').append('<p>제이쿼리로 추가한 문단</p>');});
+$(document).ready(function(){
+    $('body').append('<p>제이쿼리로 추가한 문단</p>');
+    $('a[href$="html"]').addClass('html');
+    $('a[href$="png"]').addClass('img');//아이콘만들기
+    
+    $('.tab-section').hide();
+    $('#tabs a').bind('click', function(e){
+        $('.tab-section:visible').hide();
+        $('#tabs a.current').removeClass('current').addClass('unselected');
+        $(this.hash).show();
+        $(this).removeClass('unselected').addClass('current');
+        e.preventDefault;
+    }).filter(':first').click()//탭만들기
+    
+});
+
+$(document).ready(function(){
+    externalLinks();
+});
+
+
+
+
+function externalLinks(){
+    $('a.new-window').bind('click',function(e){
+        var location = $(this).attr('href');
+        window.open(location);
+        e.preventDefault();
+    });
+}
+
+//$('a[href="../main.html"]').addClass('main');
+//$('a[href="../penance.html"]').addClass('penance');
